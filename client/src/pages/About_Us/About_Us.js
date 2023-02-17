cd63d6648d0a38d64f4eedd41a7913ebc6a9c2ad
JERRYYING02 <90186306+JERRYYING02@users.noreply.github.com>
2023-02-17 10:47:04 +0800

    About Page

--- client/src/pages/About_Us/About_Us.js
+++ client/src/pages/About_Us/About_Us.js
@@ -1,16 +1,48 @@
-import axios from "axios"
+
 import React,{useState,useContext} from 'react'
 import './About_Us.css'
-
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function About_Us() {
   return (
-    <div>
-      <h1 className="AbtUs_Main_Title">THIS IS THE ABOUT US PAGE</h1>
-      <h2 className="AbtUs_Section_Title">THIS IS SECTION TITLE</h2>
-      <h3 className="AbtUs_Para">Hmm.. not sure how to make this part start below the navbar 🤔</h3>
+    <div className="aboutpage">
+    <Container>
+      <Row>
+        <Col md={6}>
+          <Image src="https://codefile.io/_next/image?url=%2Fillustration.png&w=1200&q=75" fluid rounded />
+        </Col>
+        <Col md={6}>
+         
+          <h2>About Us</h2>
+          <p>
+           We are agile software development team (T7G7) from University of London,inspired to provide world-access of splendid recipes
+          </p>
+          <p className="member">
+            <h3>Your Web Developers  </h3>
+           
+            
+            <li>
+              Jerry Ying
+            </li>
+            <li>
+              Muhammad Naufal Al Ghifari
+            </li>
+            <li>
+              Onyx Christopher
+            </li>
+            <li>
+              Juan Sebastián Osorio Valencia
+            </li>
+            <li>
+              Takafumi Nakayama
+            </li>
+          </p>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
-}
+};
+
 
 export default About_Us
