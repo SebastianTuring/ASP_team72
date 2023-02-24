39e41cbf987b7028a3a79fe9528adfe4dc1381a0
JERRYYING02 <90186306+JERRYYING02@users.noreply.github.com>
2023-02-24 21:25:16 +0800

    recipe unknown

--- client/src/components/MealsComponent/index.js
+++ client/src/components/MealsComponent/index.js
@@ -12,7 +12,7 @@ function MealsComponent({ meals }) {
         {meals ? meals.map((meal) => (
           <MealCard key={meal.idMeal}{...meal} />
 
-        )) : <>No recipe available</>}
+        )) : <div className="unknown"><h1>No recipe available</h1></div>}
       </div>
       <Carol />
     </div>
