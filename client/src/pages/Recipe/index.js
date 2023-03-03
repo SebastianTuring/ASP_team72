cac2435e4304a3032011710749e1cdf25551bb61
tn991 <nakayama.tak.912@gmail.com>
2023-03-03 09:11:02 +0900

    layout change for recipe page

--- client/src/pages/Recipe/index.js
+++ client/src/pages/Recipe/index.js
@@ -39,11 +39,11 @@ function Recipe() {
     }
 
     const h2style = { color: "black" }
-    const containerStyle = {
-      marginTop: "7vh",
-      marginBottom: 100,
+    // const containerStyle = {
+    //   marginTop: "7vh",
+    //   marginBottom: 100,
 
-    }
+    // }
 
 
     const instructionArray = strInstructions ? strInstructions.split('.') : ["No instruction available"]
@@ -55,36 +55,44 @@ function Recipe() {
 
     return (
       <>
-        <Container style={containerStyle}>
+        {/* <Container style={containerStyle}> */}
+        <Container>
           <Row>
             <Col>
-              <div class="r-image">
+              {/* <div class="r-image"> */}
+              <div style={{ marginTop: "7vh", textAlign: "center" }}>
                 <img src={strMealThumb}></img>
+              </div>
+              {/* <h1 class="r-name">{strMeal}</h1> */}
+              <div style={{ marginLeft: "30vw" }}>
+                <h1>{strMeal}</h1>
+                <h2 style={h2style}>Ingredients</h2>
+                {/* <h2>Ingredients</h2> */}
+                {/* <ol class="p-list"> */}
+                <ul>
+                  {ingredients.map((ingredient) => (
+                    <li>{ingredient.strIngredient} : {ingredient.strMeasure}</li>
+                  ))}
+                </ul>
+
+                <h2 style={h2style}>Preparation Steps</h2>
+                {/* <ol class="p-list"> */}
+                <ol>
+                  {instructionArray.map((instruction) => (
+                    <li>{instruction}</li>
+                  ))}
+                </ol>
               </div>
 
             </Col>
-            <Col id="recipe">
-              <h1 class="r-name">{strMeal}</h1>
+            {/* <Col id="recipe">
 
-              <h2 style={h2style}>Ingredients</h2>
-              <ol class="p-list">
-                {ingredients.map((ingredient) => (
-                  <li>{ingredient.strIngredient} : {ingredient.strMeasure}</li>
-                ))}
-              </ol>
-
-              <h2 style={h2style}>Preparation Steps</h2>
-              <ol class="p-list">
-                {instructionArray.map((instruction) => (
-                  <li>{instruction}</li>
-                ))}
-              </ol>
-            </Col>
+            </Col> */}
           </Row>
-          <Row class="com-section">
+          {/* <Row class="com-section">
             <h2 style={h2style}>Comments:</h2>
             <div class="sample-com">Be the first to leave a comment!</div>
-          </Row>
+          </Row> */}
         </Container>
       </>
     );
